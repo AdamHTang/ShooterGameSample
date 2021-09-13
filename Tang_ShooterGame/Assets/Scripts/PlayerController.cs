20af7e3044754d695d67a351b8ad7d2d2a1cb8cf
AdamHTang <aht3138@utulsa.edu>
2021-09-13 16:41:38 -0500

    Player Object Created
    Started player object.
    Started PlayerController script.

---
+++ Tang_ShooterGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+/*
+ *  Created by: Adam Tang
+ *  Date Created: Sept 13, 2021
+ *  
+ *  Last Edited by:
+ *  Last Updated: Sept 13, 2021
+ *  
+ *  Description: Player control movements
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
