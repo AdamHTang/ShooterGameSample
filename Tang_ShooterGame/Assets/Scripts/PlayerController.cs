f335341b5476560690120a13156584e0955d5a6a
AdamHTang <aht3138@utulsa.edu>
2021-09-15 16:40:42 -0500

    PlayerController and BoundsLock
    Created the PlayerController script and BoundsLock script.

--- Tang_ShooterGame/Assets/Scripts/PlayerController.cs
+++ Tang_ShooterGame/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@
  *  Date Created: Sept 13, 2021
  *  
  *  Last Edited by:
- *  Last Updated: Sept 13, 2021
+ *  Last Updated: Sept 15, 2021
  *  
  *  Description: Player control movements
  */
@@ -13,15 +13,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /* VARIABLES */
+    public bool mouseLook = true;           // Are we looking at the mouse.
+    public string horzAxis = "Horizontal";
+    public string vertAxis = "Vertical";
+    public string fireAxis = "Fire1";
+    public float maxSpeed = 5f;             // Speed of the character.
 
-    // Update is called once per frame
-    void Update()
+    private Rigidbody playerRB = null;
+
+    /* Awake - Call before game start*/
+    private void Awake()
     {
-        
-    }
+        playerRB = GetComponent<Rigidbody>();   // Sets the object's rigidbody to a variable called playerRB.
+    }   // End Awake()
+
+    private void FixedUpdate()
+    {
+        float horz = Input.GetAxis(horzAxis);
+        float vert = Input.GetAxis(vertAxis);
+        Vector3 moveDirection = new Vector3(horz, 0.0f, vert);
+
+        playerRB.AddForce(moveDirection.normalized * maxSpeed);            // normalized keeps the magnitude at 1 (unit vector).
+        playerRB.velocity = new Vector3(Mathf.Clamp(playerRB.velocity.x, -maxSpeed, maxSpeed),
+                                        Mathf.Clamp(playerRB.velocity.y, -maxSpeed, maxSpeed),
+                                        Mathf.Clamp(playerRB.velocity.z, -maxSpeed, maxSpeed));
+
+        if (mouseLook)
+        {
+            Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(
+                                  new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+
+            mousePosWorld = new Vector3(mousePosWorld.x, 0.0f, mousePosWorld.z);
+
+            Vector3 lookDirection = mousePosWorld - transform.position;
+            transform.localRotation = Quaternion.LookRotation(lookDirection.normalized, Vector3.up);
+
+        }
+    }   // End FixedUpdate()
 }
